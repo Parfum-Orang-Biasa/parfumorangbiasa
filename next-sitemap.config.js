e83f3b508152e0b3b667bba90809b2f3b5705fca
Yhoga <yhogafadrian@gmail.com>
2025-06-22 17:51:39 +0700

    fix: sitemap www

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://parfumorangbiasa.com',
+    siteUrl: process.env.SITE_URL || 'https://www.parfumorangbiasa.com',
     generateRobotsTxt: true, // (optional)
+    sitemapSize: 5000,
     // ...other options
   }
